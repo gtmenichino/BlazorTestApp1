348d5bbbe0689d6e9438e88732f60302a2b88756
Garrett Menichino <gtmenichino@gmail.com>
2022-11-08 10:09:10 -0500

    Add project files.

---
+++ BlazorTestApp1/MyClass.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorTestApp1
+{
+    public class MyClass
+    {
+        public string Name;
+        public MyClass()
+        {
+            Name = "MyName"; //set initial value
+        }
+
+    }
+}
